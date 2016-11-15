93a23f65f218b5fa647440b219f0b51d0bd39c32
Samed Yalniz <s.yalniz@hotmail.de>
2016-11-15 15:25:15 +0000

    link to current stock on profile

--- spec/features/stock_feature_spec.rb
+++ spec/features/stock_feature_spec.rb
@@ -26,6 +26,21 @@ feature 'Stock list' do
 
   end
 
+  scenario"user can see link to current_stock in the profile of users" do
+    sign_up
+    add_product
+    visit "/"
+    click_link "Aktueller Lagerbestand"
+    click_link "Produkt zum Lagerbestand hinzufügen"
+    page.check "Apple"
+    click_button "Zum Lagerbestand hinzufügen"
+    click_link "Mein Profil"
+    expect(page).to have_link "Der aktuelle Lagerbestand"
+    click_link "Der aktuelle Lagerbestand"
+    expect(page).to have_content("Apple | Herkunftsland: Uruguay | Paketgröße: 100kg | Paketpreis: 10€ | Produktklasse: 1")
+
+  end
+
 
 
 end
