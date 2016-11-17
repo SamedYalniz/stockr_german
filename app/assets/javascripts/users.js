fb31dba5a8781ffa3ac99d44ec876ae2100d07b3
Samed Yalniz <s.yalniz@hotmail.de>
2016-11-17 16:06:00 +0000

    add best-in-place gem and edit user profile

--- app/assets/javascripts/users.js
+++ app/assets/javascripts/users.js
@@ -1,13 +1,16 @@
 
 $( document ).ready(function() {
-  $("li.company_information_details").hover((function() {
-    $(this).find('span.pencil').show();
-  }), function() {
-    $(this).find('span.pencil').hide();
-  });
+    $("li.company_information_details").hover((function() {
+      $(this).find('span.pencil').show();
+    }), function() {
+      $(this).find('span.pencil').hide();
+    });
 });
+
 
 $(document).ready(function() {
   /* Activating Best In Place */
   jQuery(".best_in_place").best_in_place();
+
+
 });
