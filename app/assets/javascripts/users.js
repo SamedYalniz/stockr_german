ed4a57fb5e3811c48e7d8bf85bb18f9e9c0920ba
Samed Yalniz <s.yalniz@hotmail.de>
2016-11-17 16:53:03 +0000

    improved hover over profile information

--- app/assets/javascripts/users.js
+++ app/assets/javascripts/users.js
@@ -2,8 +2,20 @@
 $( document ).ready(function() {
     $("li.company_information_details").hover((function() {
       $(this).find('span.pencil').show();
+      if($(this).find(":input").is(":focus")){
+        $(this).removeClass("Hover");
+      }
+      else {
+        $(this).addClass("Hover");
+
+      }
+      $("li.company_information_details").focusin(function() {
+        $(this).removeClass("Hover");
+      });
     }), function() {
       $(this).find('span.pencil').hide();
+      $(this).removeClass("Hover");
+
     });
 });
 
@@ -12,5 +24,10 @@ $(document).ready(function() {
   /* Activating Best In Place */
   jQuery(".best_in_place").best_in_place();
 
+
+});
+
+
+$(document).ready(function() {
 
 });
